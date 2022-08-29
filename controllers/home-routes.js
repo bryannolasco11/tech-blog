e3e4233024f785b102f7d7ea50cc7e5e68ffec66
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-29 13:59:23 -0500

    changed post title to link instead of comments.  Also, single posts are displayed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -58,6 +58,50 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'text_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include:{
+                    model:User,
+                    attributes:['username']
+                }
+            },
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({message: 'No post found with this id'});
+            return;
+        }
+        //serielize the data
+        const post = dbPostData.get({plain: true});
 
+        //pass the data to the template
+        res.render('single-post', {
+            post,
+            loggendIn: req.session.loggedIn
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
