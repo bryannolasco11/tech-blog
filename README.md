8dbf1912e143a2304cc33232c875b67e16e2bb4d
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-29 12:38:27 -0500

    I believe all the login and logout functions are working.  Pages are still not yet styled.  I am unsure if the redirects will work.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ npm install dotenv --save
 npm install bcrypt
 npm install express-session
 npm install connect-session-sequelize
+npm i express-session connect-session-sequelize
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 //GET /api/users
 router.get('/', (req, res) => {
@@ -42,15 +42,66 @@ router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
         username: req.body.username,
-        //email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            })
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
 });
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user with that username!" });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: "That's the wrong password!"});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!'});
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(()=> {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
@@ -94,5 +145,6 @@ router.delete('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
 
 module.exports = router;
