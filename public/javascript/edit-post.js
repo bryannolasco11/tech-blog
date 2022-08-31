81182551dc270e28be6f072d6874758625ac4537
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-30 23:40:28 -0500

    edit post works

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,21 @@
+
 async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value.trim();
+    const text_content = document.querySelector('input[name="text_content"]').value.trim();
+  
+    
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+       
+        title,
+        text_content
+        
       }),
       headers: {
         'Content-Type': 'application/json'
